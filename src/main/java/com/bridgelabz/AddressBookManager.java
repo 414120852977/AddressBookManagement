e22e71c943bafc87df8832ffaef38f216458b154
Ashok Mane <ashokmane81800@gmail.com>
2021-05-08 10:39:29 +0530

    [ashok] added to crate contact into addressbook

--- src/main/java/com/bridgelabz/AddressBookManager.java
+++ src/main/java/com/bridgelabz/AddressBookManager.java
@@ -1,10 +1,17 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBookManager {
     public static void main(String[] args) {
         /**
          * welcome message
          */
         System.out.println("**Welcome to AddressBook**");
+        List<Contact> list = new ArrayList();
+        Contact person = new Contact("ashok","mane","jalna","maharashtra","aurangabad","ash@gmail.com",4544,454);
+        list.add(person);
+        System.out.println(list);
     }
 }
