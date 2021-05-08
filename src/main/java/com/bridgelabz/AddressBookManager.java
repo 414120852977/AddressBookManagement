77d3b4d17176cc7400c1f74ec4686853d33af5f0
Ashok Mane <ashokmane81800@gmail.com>
2021-05-08 09:33:58 +0530

    [ashok] created welcome message

---
+++ src/main/java/com/bridgelabz/AddressBookManager.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class AddressBookManager {
+    public static void main(String[] args) {
+        /**
+         * welcome message
+         */
+        System.out.println("**Welcome to AddressBook**");
+    }
+}
